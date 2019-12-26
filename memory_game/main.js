610d11a89a57e304ef33e647e1eeb866c9c7e11e
Alex Boivin <alexboivin6248@gmail.com>
2019-12-26 12:23:58 -0700

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,4 +1,28 @@
-let cards = ['queen','queen', 'king', 'king']
+let cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png'
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png'
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png',
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'images/king-of-diamonds.png'
+}
+]
+
+
+
 let cardsInPlay = []
 
  /*if (cardsInPlay.length === 2) {
@@ -18,9 +42,11 @@ let checkForMatch = function() {
 }
 
 let flipCard = function(cardId) {
-  let cardName = cards[cardId]
-  cardsInPlay.push(cardName)
-  console.log(`User flipped ${cardName}`)
+  let cardObject = cards[cardId]
+  cardsInPlay.push(cardObject.rank)
+  console.log(`User flipped ${cardObject.rank}`)
+  console.log(`Your card image is ${cardObject.cardImage}`)
+  console.log(`The card suit is ${cardObject.suit}`)
   checkForMatch()
 }
 
@@ -28,4 +54,76 @@ flipCard(0)
 flipCard(2)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const contacts = [ {
+    firstName: "John", lastName: "Doe",
+    phone: "(512) 355‐0453", email: "johndoe@email.com"
+    }, {
+    firstName: "Jane", lastName: "Doe",
+    phone: "(312) 641‐2203", email: "janedoe@email.com"
+    }, {
+    firstName: "Suzie",
+    lastName: "Smith",
+    phone: "(415) 604‐4219",
+    email: "suziesmith@email.com"
+    } ];
+
+ let listContacts = function() {
+     for (let i =0; i < contacts.length; i++) {
+         console.log(contacts[i].firstName + ' ' + contacts[i].lastName)
+     }
+ }
+
+ listContacts()
+
+
+
+
+
+
+ const pet = {
+    name: "Daisy",
+    species: "cat",
+    favoriteToy: "laser pointer",
+    age: 5,
+    makeNoise: function () {
+      console.log("Meow!");
+    }
+  };
   
+  pet.makeNoise();
+
+
+
+let poem = {
+    author: 'Robert Frost',
+    datePublished: 1916,
+    name: 'The Road Not Taken',
+    famousLine: 'Two roads diverged in a yellow wood',
+    qoute: function() {
+        console.log(this.author)
+
+    }
+}
+
+poem.qoute()
+*/
+
+
